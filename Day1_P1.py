b86a59087bb098ac3082c9185b0cda3126ec6414
nmkgm <bala.murali.gm@gmail.com>
2019-09-23 13:18:10 +0530

    Code as on 23rd Sep Prelunch

---
+++ Day1_P1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print ('hello murali')
+
+x = 20
+y = "Okay"
+
+z = [1,2,3,4]
+zz = [z,x,y]
+print(z)
+print(zz)
+print(x,y)
---
+++ Day1_P2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import time
+
+print ("Hello")
+time.sleep(2)
+print (" Good Morning")
+time.sleep(3)
+print ("the current time is  ",time.ctime())
